77ed90d4a3fd2eba47a1e31504f9e5da372d1290
SkyWalker2506 <42008533+SkyWalker2506@users.noreply.github.com>
2024-02-18 18:43:54 +0300

    Observable class added for general use.

---
+++ Board Defence Game/Assets/Observable/Observable.cs
@@ -0,0 +1,27 @@
+using System;
+
+
+namespace Observables
+{
+    public class Observable<T> 
+    {
+        private T value;
+        public T Value
+        {
+            get => value;
+            set
+            {
+                if (value.Equals(this.value))
+                {
+                    return;
+                }
+                this.value = value;
+                OnValueChanged?.Invoke(value);
+            }
+        }
+        public Action<T> OnValueChanged;
+    }
+}
+
+
+
